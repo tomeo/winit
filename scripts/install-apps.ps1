082da012a14e46fb6d27739ab7c94cb97f23e942
Tommy Ivarsson <tommy.ivarsson@gmail.com>
2021-12-19 00:07:56 +0100

    Add nodejs and jq

--- scripts/install-apps.ps1
+++ scripts/install-apps.ps1
@@ -2,3 +2,4 @@ scoop install googlechrome
 scoop install slack
 scoop install discord
 scoop install vlc
+scoop install jq
---
+++ scripts/install-nodejs.ps1
@@ -0,0 +1,4 @@
+scoop install nodejs
+npm i -g md-to-pdf
+npm i -g elasticdump
+npm i -g yarn
