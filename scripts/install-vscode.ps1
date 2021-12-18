318f69290172b1566637b6cb534933db2c853ec4
Tommy Ivarsson <tommy.ivarsson@gmail.com>
2021-12-19 00:14:07 +0100

    Add extensions to VSCode

--- scripts/install-vscode.ps1
+++ scripts/install-vscode.ps1
@@ -1,3 +1,8 @@
 scoop install vscode
 code --install-extension DavidAnson.vscode-markdownlint
 code --install-extension humao.rest-client
+code --install-extension phplasma.csv-to-table
+code --install-extension EditorConfig.EditorConfig
+code --install-extension SimonSiefke.svg-preview
+code --install-extension adamhartford.vscode-base64
+code --install-extension neilding.language-liquid
