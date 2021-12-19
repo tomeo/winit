c0271782c5fa6e66f111110cadafea26312c72ba
Tommy Ivarsson <tommy.ivarsson@gmail.com>
2021-12-19 01:15:06 +0100

    Set VSCode settings

---
+++ scripts/configure-vscode-settings.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+
+const path = `${process.env.APPDATA}/Code/User/settings.json`;
+
+fs.readFile(path, 'utf8' , (err, data) => {
+  let settings = JSON.parse(data || '{}');
+
+  settings['editor.formatOnSave'] = false;
+  settings['editor.minimap.enabled'] = false;
+  settings['editor.largeFileOptimizations'] = false;
+  settings['editor.fontFamily'] = 'FiraCode NF';
+  settings['editor.fontLigatures'] = true;
+  settings['editor.fontSize'] = 11;
+  settings['editor.renderWhitespace'] = 'all';
+
+  settings['files.eol'] = '\n';
+
+  settings['javascript.updateImportsOnFileMove.enabled'] = 'always';
+
+  settings['workbench.colorTheme'] = 'Solarized Dark';
+  settings['workbench.editor.showIcons'] = false;
+  settings['workbench.activityBar.visible'] = false;
+  
+  settings['window.menuBarVisibility'] = 'default';
+  settings['window.zoomLevel'] = 1;
+
+  fs.writeFileSync(path, JSON.stringify(settings, null, 2));
+})
--- scripts/install-vscode.ps1
+++ scripts/install-vscode.ps1
@@ -6,3 +6,6 @@ code --install-extension EditorConfig.EditorConfig
 code --install-extension SimonSiefke.svg-preview
 code --install-extension adamhartford.vscode-base64
 code --install-extension neilding.language-liquid
+
+scoop install nodejs
+node ./configure-vscode-settings.js
