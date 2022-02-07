03c50bafde7c9239d21d8232d2df57e72b462faf
Tommy Ivarsson <tommy.ivarsson@gmail.com>
2022-02-07 08:21:17 +0100

    Added: scripts/install-azure-cli.ps1

---
+++ scripts/install-azure-cli.ps1
@@ -0,0 +1,3 @@
+scoop install azure-cli
+az extension add --source https://workerappscliextension.blob.core.windows.net/azure-cli-extension/containerapp-0.2.2-py2.py3-none-any.whl
+az provider register --namespace Microsoft.Web
