a4a8a1ffe13d5e93126df4e1853ab84fc997fdec
Tommy Ivarsson <tommy.ivarsson@gmail.com>
2024-09-02 14:22:38 +0200

    Install Windows Terminal using scoop

--- scripts/configure-windows-terminal.ps1
+++ scripts/configure-windows-terminal.ps1
@@ -1,3 +1,3 @@
-copy apps/windows-terminal/settings.json "$env:LOCALAPPDATA/Microsoft/Windows Terminal/settings.json"
+copy ../apps/windows-terminal/settings.json "C:\Users\$env:USERNAME\scoop\apps\windows-terminal\current\settings\settings.json"
 
 scoop install https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/oh-my-posh.json
