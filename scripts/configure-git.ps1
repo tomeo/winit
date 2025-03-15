2c1ef299903df3c3e04d31909cc50d45b1663881
Tommy Ivarsson <tommy.ivarsson@gmail.com>
2025-03-15 23:23:29 +0100

    add diff-so-fancy

--- scripts/configure-git.ps1
+++ scripts/configure-git.ps1
@@ -1,8 +1,20 @@
-$Email = Read-Host -Prompt 'Git e-mail'
-$FullName = Read-Host -Prompt 'Git full name'
+# Check if Git user email is already set
+$Email = git config --global user.email
+$FullName = git config --global user.name
 
-git config --global user.email $Email
-git config --global user.name $FullName
+if (-not $Email) {
+    $Email = Read-Host -Prompt 'Git e-mail'
+    git config --global user.email $Email
+} else {
+    Write-Host "Git e-mail is already set to: $Email"
+}
+
+if (-not $FullName) {
+    $FullName = Read-Host -Prompt 'Git full name'
+    git config --global user.name $FullName
+} else {
+    Write-Host "Git full name is already set to: $FullName"
+}
 
 git config --global alias.co checkout
 git config --global alias.br branch
@@ -18,3 +30,24 @@ git config --global core.excludesfile ~/.gitignore
 git config --global core.autocrlf true
 git config --global core.safecrlf false
 Add-Content -Path $env:USERPROFILE/.gitignore -Value '.hidden'
+
+# diff so fancy
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
+git config --global interactive.diffFilter "diff-so-fancy --patch"
+git config --global color.ui true
+
+# Color Settings for better visibility
+git config --global color.diff-highlight.oldNormal    "red bold"
+git config --global color.diff-highlight.oldHighlight "red bold 52"
+git config --global color.diff-highlight.newNormal    "green bold"
+git config --global color.diff-highlight.newHighlight "green bold 22"
+
+git config --global color.diff.meta       "yellow bold"
+git config --global color.diff.frag       "magenta bold"
+git config --global color.diff.func       "blue bold"
+git config --global color.diff.commit     "cyan bold"
+git config --global color.diff.old        "red bold"
+git config --global color.diff.new        "green bold"
+git config --global color.diff.whitespace "red reverse"
+
+Write-Host "Git has been configured successfully!" -ForegroundColor Green
--- scripts/install-nodejs.ps1
+++ scripts/install-nodejs.ps1
@@ -4,3 +4,4 @@ scoop install nodejs-lts
 npm i -g md-to-pdf
 npm i -g elasticdump
 npm i -g yarn
+npm i -g diff-so-fancy
